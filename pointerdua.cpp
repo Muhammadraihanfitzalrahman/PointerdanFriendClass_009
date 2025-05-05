48dd470e0cbef67444ed8e772c3c303722405cf2
muhammadraihanfitzalrahman <Ayamtulebo19@gmail.com>
2025-05-05 11:03:49 +0700

    int main

--- pointerdua.cpp
+++ pointerdua.cpp
@@ -8,3 +8,12 @@ class mahasiswa {
         cout << "No induk mahasiswa : " << nim << endl;
     };
 };
+
+int main(){
+    mahasiswa *mhs = mahasiswa; // pointer to class object
+    mhs->nim = 1234567890; // Accessing class member using pointer
+    mhs->showNim(); // calling class method using pointer
+
+    delete mhs; // freeing allocated memory
+    return 0;
+};
