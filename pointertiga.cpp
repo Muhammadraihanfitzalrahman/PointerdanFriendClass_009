3b884cafc878c9f28e02c62e50c46d7e13d96245
muhammadraihanfitzalrahman <Ayamtulebo19@gmail.com>
2025-05-05 11:11:27 +0700

    using namespace

--- pointertiga.cpp
+++ pointertiga.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 #include <string>
+
+using namespace std;
+
+class siswa;
+class orang;
+{
+    private:
+    string nama;
+    public:
+    void setNama(string pNama);
+    friend class siswa; // Declare class siswa as a friend of class orang
+};l
